78c5c44bc8204a332f481eec4ada2316774368fe
rafita75 <estudiosjuarez24@gmail.com>
2026-04-14 19:35:26 -0600

    cookies es dashboardLayour

--- frontend/src/modules/dashboard/components/DashboardLayout.jsx
+++ frontend/src/modules/dashboard/components/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 import { Link, Outlet } from "react-router-dom";
 import { clearAuthCookies } from '../../../lib/cookies';
 import {
